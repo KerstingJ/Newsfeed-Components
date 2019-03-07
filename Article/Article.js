db9afef3d41505b150398ad0f7b8b7e0be4b2503
KerstingJ <Kersting.Josh@Gmail.com>
2019-03-06 19:14:21 -0500

    got rid of lag in minimize transition

--- Article/Article.js
+++ Article/Article.js
@@ -4,19 +4,24 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    domElement.style.transition = "2s";
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", this.expandArticle.bind(this));
-    
+
+    // the order is important
+    this.height = parseInt(window.getComputedStyle(this.domElement).height.split("p")[0]);
+    this.height += 50; 
+    this.domElement.style.maxHeight = '50px';
+    this.domElement.style.transition = "2s";
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
+    this.domElement.style.maxHeight = this.domElement.classList.contains("article-open") ? `${this.height}px` : '50px';
   }
 
   static fromObj(obj){
@@ -27,6 +32,7 @@ class Article {
     time.classList.add("date");
     time.textContent = obj.date || new Date().toDateString();
 
+    // to make this work for multiple p's maybe have the paragraphs in an array then hit em with that forEach.
     const content = document.createElement("p");
     content.textContent = obj.content;
 
@@ -37,7 +43,7 @@ class Article {
     const article = document.createElement("div");
     article.classList.add("article");
 
-    article.append(h2, time, content, expand); // so apparently this works but its not as nice as appendChild... 
+    article.append(h2, time, content, expand); // so apparently this works
 
     return article;
   }
@@ -53,7 +59,7 @@ class Article {
 
 const articleObj = {
   heading: "My Super Awesome Heading",
-  content: "lorem ipsum and alluh that",
+  content: "lorem ipsum and alluh that. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pidgeotto Lorem ipsum dolor sit amet, consectetur adipiscin elit. Pidgeot Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rattata Lorem ipsum dolor sit amet, consectetur adipiscingelit. Raticate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Spearow Lorem ipsum dolor sit amet, consectetur adipiscingelit. Fearow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ekans Lorem ipsum dolor sit amet, consectetur adipiscingelit. Arbok Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pikachu Lorem ipsum dolor sit amet, consectetur adipiscingelit. Raichu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sandshrew Lorem ipsum dolor sit amet, consectetur adipiscingelit. Sandslash Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consecteturadipiscing elit. Nidorina Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoqueen Lorem ipsum dolor sit amet,consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolorsit amet, consectetur adipiscing elit. Nidoking Lorem ipsum",
 };
 
 const articleContainer = document.querySelector(".articles");
